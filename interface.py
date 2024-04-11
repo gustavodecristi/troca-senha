28d33c7a47a87b5165134e795637d777e8720d59
Gustavo <gustavodecristi@outlook.com>
2024-04-11 15:18:15 -0300

    Versão 1.0

---
+++ interface.py
@@ -0,0 +1,83 @@
+import PySimpleGUI as sg
+import troca_senha
+
+sg.theme("Reddit")
+layout = [
+    [sg.Text("TROCAR SENHA - SGU CARD", font=15)],
+    [sg.Text("Usuário", font=7)],
+    [sg.InputText(key="usuario", size= 200, font=3)],
+    [sg.Text("Senha", font=7)],
+    [sg.InputText(key="senha", password_char='*', size= 200)],
+    [sg.Text("Confirmar senha", font=7)],
+    [sg.InputText(key="confirma_senha", password_char='*', size= 200)],
+    [sg.Button("Confirmar", bind_return_key=True), sg.Button("Limpar")],
+    [sg.Text("Após clicar em confirmar, aguarde a mensagem de concluído.", key="resultado")]
+]
+janela = sg.Window("Troca Senha", layout, resizable=True, size=(400, 280))
+
+
+def open_window(user, password, confirm_password):
+    sg.theme("LightBlue6")
+    layout2 = [
+        [sg.Text("Por favor aguarde.", font=20)]
+    ]
+    window = sg.Window("Processando...", layout2, modal=True, finalize=True, disable_minimize=True, disable_close=True)
+    window.read(10)
+    pega_status = troca_senha.troca_senha(user, password, confirm_password)
+    window.close()
+    return pega_status
+
+
+
+while True:
+    evento, valores = janela.read()
+
+    usuario = valores["usuario"]
+    senha = valores["senha"]
+    confirma_senha = valores["confirma_senha"]
+
+    if evento == sg.WIN_CLOSED:
+        break
+
+    elif evento == "Limpar":
+        janela["usuario"].update("")
+        janela["senha"].update("")
+        janela["confirma_senha"].update("")
+        janela["resultado"].update("Após clicar em confirmar, aguarde a mensagem de concluído.")
+
+    elif evento == "Confirmar":
+        if valores["usuario"] == "" or valores["senha"] == "" or valores["confirma_senha"] == "":
+            janela["resultado"].update("Preencha todos os campos acima")
+        elif valores["senha"] != valores["confirma_senha"]:
+            janela["resultado"].update("As senhas não conferem")
+        elif len(valores["senha"]) < 6:
+            janela["resultado"].update("A senha deve ter pelo menos 6 caracteres: ")
+        elif valores["senha"].isalpha():
+            janela["resultado"].update("A senha deve ter pelo menos um número")
+        else:
+            '''
+            Lista de erros:
+            001: Problema ao abrir Card Admin (função get)
+            002: Problema ao fazer login, pode ser que a senha tenha expirado
+            003: Erro ao pesquisar usuário
+            004: Usuário não encontrado, provavelmente está inativo
+            005: Erro na tela de trocar senha
+            006: Erro não especificado, sendo necessário analisar a execução do código
+            '''
+            status = open_window(usuario, senha, confirma_senha)
+            if status == "Sem erro":
+                janela["resultado"].update("Senha alterada com sucesso.")
+            elif status == "001":
+                janela["resultado"].update("ERRO001: Problema ao abrir sistema.\nEntre em contato com o TI.")
+            elif status == "002":
+                janela["resultado"].update("ERRO002: Problema ao fazer login.\nEntre em contato com o TI.")
+            elif status == "003":
+                janela["resultado"].update("ERRO003: Problema ao pesquisar usuário.\nEntre em contato com o TI.")
+            elif status == "004":
+                janela["resultado"].update("ERRO004: Usuário inativo ou inexistente.\nEntre em contato com o TI.")
+            elif status == "005":
+                janela["resultado"].update("ERRO005: Problema ao trocar senha.\nEntre em contato com o TI.")
+            else:
+                janela["resultado"].update("ERRO006: Erro não especificado.\nEntre em contato com o TI.")
+
+janela.close()
---
+++ troca_senha.py
@@ -0,0 +1,80 @@
+# Importação das bibliotecas a serem usadas no software
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import TimeoutException
+from config import link_sistema, login, senha
+
+'''
+A função abaixo vai servir para trocar a senha do usuário dentro do SGU Card.
+Utilizando o Selenium (biblioteca de automação Python), o "robô" vai abrir o sistema, infomar login, senha
+e fazer o acesso. Depois disso vai seguir o caminho até chegar na tela de pesquisa de usuários, quando isso acontecer,
+ele vai receber o usuário desejado como parâmetro, realizar a pesquisa e trocar a senha.
+'''
+
+
+def troca_senha(user, passwd, confirm_passwd):  # Parâmetros: Usuário, Senha e Confirmação da senha
+    options = Options()
+    options.add_argument("--headless")
+
+    navegador = webdriver.Chrome(
+        executable_path=ChromeDriverManager().install(),
+        options=options,
+        service_args=["--silent"] # Suprime a saída do serviço do WebDriver
+    )
+
+    erro = []
+
+    try:
+        # Entrando no link e fazendo login.
+        navegador.get(link_sistema)
+        navegador.maximize_window()
+        
+    except:
+        erro.append("001")
+
+    try:
+
+        navegador.find_element(By.XPATH, '//*[@id="login"]').send_keys(login)  # Usuário
+        navegador.find_element(By.XPATH, '//*[@id="passwordTemp"]').send_keys(senha)  # Senha
+        navegador.find_element(By.XPATH, '//*[@id="Button_DoLogin"]').click()  # Botão de entrar
+        WebDriverWait(navegador, 3).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="conteudo"]/table/tbody/tr[34]/td[1]/a')))  # Função de espera, serve para esperar a página carregar
+
+    except:
+        erro.append("002")
+
+    try:
+        navegador.find_element(By.XPATH, '//*[@id="conteudo"]/table/tbody/tr[34]/td[1]/a').click()  # Acessar página de usuários
+
+        WebDriverWait(navegador, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="conteudo"]/form/table/tbody/tr[5]/td/input')))
+        navegador.find_element(By.XPATH, '//*[@id="conteudo"]/form/table/tbody/tr[2]/td[2]/input').send_keys(user)  # Preenche o campo de pesquisa com o parâmetro 'User'
+        navegador.find_element(By.XPATH, '//*[@id="conteudo"]/form/table/tbody/tr[5]/td/input').click()  # Botão pesquisar
+
+    except:
+        erro.append("003")
+
+    try:
+        WebDriverWait(navegador, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="conteudo"]/table/tbody/tr[2]/td[8]/span[1]/a/img')))
+        navegador.find_element(By.XPATH, '//*[@id="conteudo"]/table/tbody/tr[2]/td[8]/span[1]/a/img').click()  # Acessa a página de senha do usuário
+
+        WebDriverWait(navegador, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="conteudo"]/form/table/tbody/tr[10]/td/input[2]')))
+        navegador.find_element(By.XPATH, '//*[@id="conteudo"]/form/table/tbody/tr[9]/td[2]/input[2]').click()  # Marca o campo 'Bloqueado' como 'Não'
+
+        Select(navegador.find_element(By.ID, 'FG_SITUACAO')).select_by_visible_text('Sim')  # Seleciona a opção de trocar a senha no próximo acesso
+
+        navegador.find_element(By.XPATH, '//*[@id="conteudo"]/form/table/tbody/tr[7]/td[4]/input').send_keys(passwd)  # Campo de senha que recebe o parâmetro 'passwd'
+        navegador.find_element(By.XPATH, '//*[@id="conteudo"]/form/table/tbody/tr[8]/td[4]/input').send_keys(confirm_passwd)  # Campo de confirmação que recebe o parâmetro 'confirm_passwd'
+        navegador.find_element(By.XPATH, '//*[@id="conteudo"]/form/table/tbody/tr[10]/td/input[2]').click()  # Botão confirmar
+
+    except TimeoutException:
+        erro.append("004")
+
+    except:
+        erro.append("005")
+
+    erro.append("Sem erro")
+    return erro[0]
